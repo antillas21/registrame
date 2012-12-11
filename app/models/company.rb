ad55fba7745b87683cf969eb1f1c4a2a411adb40
Antonio Antillon <antillas21@gmail.com>
2012-12-10 23:01:54 -0800

    Company no longer belongs to :sector, :state, :country

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,32 +3,5 @@ class Company < ActiveRecord::Base
   validates_uniqueness_of :name
   
   has_many :users, :dependent => :destroy
-  belongs_to :country
-  belongs_to :state
-  belongs_to :sector
-  
-  def state_name
-    state.name if state
-  end
-  
-  def state_name=(name)
-    self.state = State.find_or_create_by_name(name) unless name.blank?
-  end
-  
-  def country_name
-    country.name if country
-  end
-  
-  def country_name=(name)
-    self.country = Country.find_or_create_by_name(name) unless name.blank?
-  end
-  
-  def sector_name
-    sector.name if sector
-  end
-  
-  def sector_name=(name)
-    self.sector = Sector.find_or_create_by_name(name) unless name.blank?
-  end
-    
+
 end
